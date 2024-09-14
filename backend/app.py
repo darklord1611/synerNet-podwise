15d6710d0c572a525ff2e2ceb53631204902989f
christopher1611 <tavietanh16112004@gmail.com>
2024-09-14 16:32:02 +0700

    chore: restructure

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,21 @@
+import json
 import sys
-from dotenv import load_dotenv
 import os
 sys.path.insert(0, os.path.realpath(os.path.pardir))
-from fastapi import FastAPI, File, UploadFile, Body
+from fastapi import FastAPI, File, HTTPException, UploadFile, Body
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from celery_tasks.tasks import predict_image
-from celery.result import AsyncResult
-from api.models import Prediction, Task
-from services.audio_service.YoutubeLoader import download_audio_from_youtube
-from services.audio_service.Transcript import transcribe_audio
+
+
+# from services.audio_service.YoutubeLoader import download_audio_from_youtube
+# from services.audio_service.Transcript import transcribe_audio
 import uuid
 import logging
 from typing import List
-import numpy as np
+
 from supabase import create_client, Client
+from config import SUPABASE_URL, SUPABASE_KEY, APP_HOST, APP_PORT
 
 UPLOAD_FOLDER = 'uploads'
 STATIC_FOLDER = 'static/results'
@@ -30,11 +30,8 @@ if not isdir:
 
 origins = ["*"]
 
-load_dotenv()
 
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=STATIC_FOLDER), name="static")
@@ -50,64 +47,25 @@ app.add_middleware(
 async def index():
     return {'message': 'Hello World'}
 
+
+@app.post("/api/upload")
+async def process_input()
+
 @app.post("/api/transcript")
 async def get_transcript(url: str = Body(..., embed=True)):
-    try:
-        print(url)
-        audio_path = download_audio_from_youtube(url)
-        formatted_results = await transcribe_audio(audio_path)
+    # try:
+    #     print(url)
+    #     audio_path = download_audio_from_youtube(url)
+    #     formatted_results = await transcribe_audio(audio_path)
 
-        with open("formatted_response.json", "w") as f:
-            json.dump({"results": {"utterances": formatted_results}}, f, indent=4)
+    #     with open("formatted_response.json", "w") as f:
+    #         json.dump({"results": {"utterances": formatted_results}}, f, indent=4)
 
-        return {"status": "success", "transcript": formatted_results}
+    #     return {"status": "success", "transcript": formatted_results}
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
-
-
-# @app.post('/api/process')
-# async def process(files: List[UploadFile] = File(...)):
-#     tasks = []
-#     try:
-#         for file in files:
-#             d = {}
-#             try:
-#                 name = str(uuid.uuid4()).split('-')[0]
-#                 ext = file.filename.split('.')[-1]
-#                 file_name = f'{UPLOAD_FOLDER}/{name}.{ext}'
-#                 with open(file_name, 'wb+') as f:
-#                     f.write(file.file.read())
-#                 f.close()
-
-#                 # start task prediction
-#                 task_id = predict_image.delay(os.path.join('api', file_name))
-#                 d['task_id'] = str(task_id)
-#                 d['status'] = 'PROCESSING'
-#                 d['url_result'] = f'/api/result/{task_id}'
-#             except Exception as ex:
-#                 logging.info(ex)
-#                 d['task_id'] = str(task_id)
-#                 d['status'] = 'ERROR'
-#                 d['url_result'] = ''
-#             tasks.append(d)
-#         return JSONResponse(status_code=202, content=tasks)
-#     except Exception as ex:
-#         logging.info(ex)
-#         return JSONResponse(status_code=400, content=[])
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
 
 
-# @app.get('/api/result/{task_id}', response_model=Prediction)
-# async def result(task_id: str):
-#     task = AsyncResult(task_id)
-
-#     # Task Not Ready
-#     if not task.ready():
-#         return JSONResponse(status_code=202, content={'task_id': str(task_id), 'status': task.status, 'result': ''})
-
-#     # Task done: return the value
-#     task_result = task.get()
-#     result = task_result.get('result')
-#     return JSONResponse(status_code=200, content={'task_id': str(task_id), 'status': task_result.get('status'), 'result': result})
 
 
